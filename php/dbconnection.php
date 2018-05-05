1f3d33c8ac96bc2d2a1c2cababd1be3e1fa0ed75
ebenezergraham <egraham15@alustudent.com>
2018-05-05 21:08:16 +0400

    added logo and php query for db 21:08

--- php/dbconnection.php
+++ php/dbconnection.php
@@ -7,18 +7,15 @@ $password="mysql";
 $dbname="minezon";
 $product_id;
 
-//$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-//	or die ('Could not connect to the database server' . mysqli_connect_error());
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+// get our products
+$our_product = "SELECT product_name FROM product_categories WHERE id = $x ";
+mysqli_query($con,$our_product);
+
+// set our products
+mysqli_query($con,"INSERT INTO product_categories VALUES ('1','Stones-test','20','4','images/test.jpg')");
+
 //$con->close();
-
-try {
-    $connectDB = new PDO( "mysql:host=$servername;dbname=$dbname", $username, $password );
-} catch ( PDOException $e ) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
-$prepareSQL = $connectDB->prepare( "INSERT INTO cart VALUES ('','$product_id')" );
-$prepareSQL->execute();
-$connectDB = null;
-
 ?>
